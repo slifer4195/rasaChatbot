e88c7db14207670cc1796bf88ebb900b0546b5cb
Ganeshprasad Biradar <biradarganesh25@gmail.com>
2022-03-20 19:20:44 -0500

    Added support to show bot typing status before giving the response to make it more realistic
    The actual response is also delayed in propotion
    to the message length to make it more realistic

--- slack_app.py
+++ slack_app.py
@@ -9,18 +9,35 @@ import aiohttp
 import sqlite_db as sdb
 from datetime import datetime
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 
 # Install the Slack app and get xoxb- token in advance
 app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 
-@app.event("message")
-async def say_hello(event, say):
-    logging.debug("got message")
+@app.event({
+    "type":"message",
+    "subtype": None
+})
+async def say_hello(event, say, context):
+    # logging.error("\n\n\n\n\n\n\ngot message")
     async with aiohttp.ClientSession() as session:
         async with session.post('http://localhost:5005/webhooks/rest/webhook',json={"sender":event['user'],"message":event["text"]}) as response:
             
     # r = await requests.post("http://localhost:5005/webhooks/rest/webhook",json={"sender":event['user'],"message":event["text"]})
+            token = context.bot_token
+            channel = event['user']
+            logging.error(channel)
+            text = "typing... "
+            resp = await app.client.chat_postMessage(
+                token=token, 
+                channel=channel,
+                text=text
+            )
+            # logging.error("\n\n\n\n\n\n\n\n\n\n\n\n")
+            # print(dir(resp))
+            ts = resp['message']['ts']
+            logging.error(ts)
+            # resp = event.chat_postMessage()
             json_data = json.loads(await response.text())[0]
             db = await sdb.get_db_object()
             query="""
@@ -29,7 +46,19 @@ async def say_hello(event, say):
             await db.execute(query, (event['user'],event['text'],json_data['text'],datetime.now()))
             await db.commit()
 
-            await say(f"{json_data['text']}")
+            text = json_data['text']
+            channel = resp['channel']
+
+            time_to_sleep = len(text)*0.01
+            await asyncio.sleep(time_to_sleep+0.5)
+
+            await app.client.chat_update(
+                token=token,
+                channel=channel,
+                ts=ts,
+                text=text
+            )
+
 
 
 # Add middleware / listeners here
